90595735eb440032d89d7a351bd996f6fbec4d8e
Nicolas <cailletn@northlands.edu.ar>
2020-09-09 16:36:50 -0300

    Comienzo de Henry App

---
+++ ecommerce-template-master/client/src/components/Input_Prueba/Input.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Input(){
+
+
+    return(
+        <div>
+            <form>
+                <input type="text" placeholder="Nombre" />
+                <input type="password" placeholder="Password" />           
+            </form>
+        </div>
+    )
+}
