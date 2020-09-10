fd5be91d25347f266df2f8b67c7f4a13d67a41a1
Nahue93 <nahuer.93@gmail.com>
2020-09-10 17:39:00 -0300

    Configuracion inicial de rutas y componentes principales

--- ecommerce-template-master/client/src/App.js
+++ ecommerce-template-master/client/src/App.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Route } from "react-router-dom";
 import Input from './components/Input_Prueba/Input'
+import HomeUser from './components/Input_Prueba/HomeUser';
 
 function App() {
   return (
     <div className="App">
-      <Input />
+
+      <Route exact path="/">
+        <Input />
+      </Route>
+
+      {/* -- Sin el Login el Usuario no va a poder hacer NADA-- */}
+      <Route path="/">
+        <NavBar />
+      </Route>
+      {/* Mas adelante vamos a poner el HomeUser en path="/" */}
+      <Route exact path="/HomeUser">
+        <HomeUser />
+      </Route>
+      <Route exact path="/MiPerfil">
+        <MiPerfil />
+      </Route>
+      <Route exact path="/Modulo/:id">
+        <Modulo />
+      </Route>
+      <Route exact path="/Admin">
+        <Admin />
+      </Route>
+      <Route exact path="/data">
+        {/* Va a mostrar la informacion de todos los alumnos. Va a tener filtros por cohortes y pm */}
+        <Info /> {/* Buscar nombre adecuado */}
+      </Route>
+      <Route exact path="/MiEquipo">
+        <MiEquipo />
+      </Route>
+
     </div>
   );
 }
