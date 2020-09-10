8cbd1b6323530cf38d0bb64a3861d61bfaa3fd21
Nahue93 <nahuer.93@gmail.com>
2020-09-10 17:43:33 -0300

    Componentes principales y rutas

---
+++ ecommerce-template-master/client/src/components/Input_Prueba/Admin.jsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom'
+
+export default function HomeUser(props) {
+
+    return (
+        <div><p>Soy el componente que utiliza el Administrador</p></div>
+
+    );
+}
---
+++ ecommerce-template-master/client/src/components/Input_Prueba/Modulo.jsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom'
+
+export default function HomeUser(props) {
+
+    return (
+        <div><p>Soy un Modulo en particular (M1, M2 ,M3 o M4)</p></div>
+
+    );
+}
