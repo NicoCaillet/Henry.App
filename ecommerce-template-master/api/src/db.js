9894f46b664c402999b0096ff2786fb437da06bd
franco troncoso <frantronco11@gmail.com>
2020-09-14 17:53:18 -0300

    ruta pair

--- ecommerce-template-master/api/src/db.js
+++ ecommerce-template-master/api/src/db.js
@@ -32,7 +32,7 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Usuario, Cohorte, Grupo, Notas, Clase } = sequelize.models;
+const { Usuario, Cohorte, Grupo, Notas, Clase, Pair } = sequelize.models;
 
 // Aca vendrian las relaciones
 Usuario.belongsTo(Cohorte);
@@ -45,6 +45,12 @@ Usuario.hasMany (Notas);
 Notas.belongsTo (Usuario);
 Clase.belongsTo (Cohorte);
 Cohorte.hasMany (Clase);
+Pair.hasMany(Usuario);
+Usuario.belongsTo(Pair);
+Cohorte.hasMany(Pair);
+Pair.belongsTo(Cohorte);
+Grupo.hasMany(Pair);
+Pair.belongsTo(Grupo);
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
