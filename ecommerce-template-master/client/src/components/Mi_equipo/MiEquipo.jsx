4b8223f3e52de7b8b663d87192a72897cfa3c576
Nicolas <cailletn@northlands.edu.ar>
2020-09-14 12:43:24 -0300

    Mi team terminado

--- ecommerce-template-master/client/src/components/Mi_equipo/MiEquipo.jsx
+++ ecommerce-template-master/client/src/components/Mi_equipo/MiEquipo.jsx
@@ -1,10 +1,62 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom'
+import s from "./miequipo.module.css"
+import Nav from "./componentes/nav"
+import Student from "./componentes/student"
+import Title from "./componentes/title"
+
+const student = [
+    {
+      name: "Nicolas",
+      lastname: "Caillet Bois",
+      age: 18,
+      email: "nicolas@gmail.com"
+    },
+    {
+      name: "Franco",
+      lastname: "Bonnahon",
+      age: 18,
+      email: "franco@gmail.com"
+    },
+    {
+      name: "Gianni",
+      lastname: "Pisani",
+      age: 18,
+      email: "destroyer@gmail.com"
+    },
+    {
+        name: "Gianni",
+        lastname: "Pisani",
+        age: 18,
+        email: "destroyer@gmail.com"
+      },
+      {
+        name: "Gianni",
+        lastname: "Pisani",
+        age: 18,
+        email: "destroyer@gmail.com"
+      },
+      {
+        name: "Gianni",
+        lastname: "Pisani",
+        age: 18,
+        email: "destroyer@gmail.com"
+      },
+  ]
 
 export default function MiEquipo(props) {
 
+
     return (
-        <div><p>Soy Mi Equipo, muestro la informacion del grupo del alumno</p></div>
+        <div className={s.container}>
+        <Nav /> 
+        <Title />
+        <Student student={student} />
+        {student.map((student) => (
+          <Student
+           student={student} />
+        ))}
+        </div>
 
     );
 }
