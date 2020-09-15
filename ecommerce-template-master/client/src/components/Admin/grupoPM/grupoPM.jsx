9634b99b841dd736162aafa331e9cd68ad0d423e
edijr92 <edinsonjr92@gmail.com>
2020-09-15 10:41:39 -0300

    componente crear grupo

--- ecommerce-template-master/client/src/components/Admin/grupoPM/grupoPM.jsx
+++ ecommerce-template-master/client/src/components/Admin/grupoPM/grupoPM.jsx
@@ -1,12 +1,31 @@
 import React, { useState, useEffect } from 'react';
+import s from './grupo.module.css';
+import TextField from '@material-ui/core/TextField';
+import Estudiantes from './estudiante'
 import { Link } from 'react-router-dom'
 
 export default function Admin(props) {
 
     return (
         <div>
-            <p> Grupo PM </p>
-        
+            <div className={s.admin} >
+                <div className={s.aside}>
+                    <h3> Buscar estudiante</h3>
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        label="Nombre y Apellido"
+                        fullWidth
+                        type="text"
+                        id="numero"
+                        name="numero"
+                        autoFocus />
+                </div>
+            </div>
+            <div>
+                <Estudiantes />
+            </div>
         </div>
 
     );
