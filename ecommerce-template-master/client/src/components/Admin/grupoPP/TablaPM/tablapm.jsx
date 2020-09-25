29392b26b5120609059a7feca8fcb9b7c12b4904
Francis Ricle <francisricleruiz@gmail.com>
2020-09-25 15:10:24 -0300

    corrijo estilos admin

--- ecommerce-template-master/client/src/components/Admin/grupoPP/TablaPM/tablapm.jsx
+++ ecommerce-template-master/client/src/components/Admin/grupoPP/TablaPM/tablapm.jsx
@@ -118,7 +118,7 @@ export default function CrudAlumnos() {
   if (gruposPM.length)
     return (
       <div>
-        <TableContainer component={Paper} style={{ width: '82%' }} className={s.container}>
+        <TableContainer component={Paper} className={s.container}>
           <TableHead>
             <TableRow>
               {Object.keys(gruposPM[0]).map(key => (
