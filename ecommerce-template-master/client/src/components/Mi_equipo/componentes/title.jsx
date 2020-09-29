4476f9c1e64907597c375237bc441323edf3139d
edijr92 <edinsonjr92@gmail.com>
2020-09-28 22:14:25 -0300

    anadiendo reviews

--- ecommerce-template-master/client/src/components/Mi_equipo/componentes/title.jsx
+++ ecommerce-template-master/client/src/components/Mi_equipo/componentes/title.jsx
@@ -4,15 +4,14 @@ import { Link } from 'react-router-dom'
 import s from './title.module.css'
 
 export default function Title() {
-    return (
-      <div className={s.grid}>
-        <div className={s.letra}> Nombre </div>
-        <div className={s.letra}> Apellido </div>
-        <div className={s.letra}> Edad </div>
-        <div className={s.letra}> Localidad </div>
-        <div className={s.letra}> Mail </div>
+  return (
+    <div className={s.grid}>
+      <div className={s.letra}> Nombre </div>
+      <div className={s.letra}> Apellido </div>
+      <div className={s.letra}> Localidad </div>
+      <div className={s.letra}> Mail </div>
+      <div className={s.letra}> Reviews </div>
+    </div>
+  );
+}
 
-      </div>
-    );
-  }
-  
